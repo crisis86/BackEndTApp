fb5a8012ad2853d7bc4f6004a81d496ff3e5a709
DavideFranzys <webmaster@franzysonline.it>
2023-09-07 10:21:32 +0200

    change

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.$produrl = "https://tennissapp.onrender.com";
+window.$devurl = "http://localhost:10000";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
